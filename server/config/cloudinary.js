9c3933a9de39e345a2aaded0fe052ab1b38e6876
Yash Sabne <yashsabne39@gmail.com>
2025-02-09 12:56:47 +0530

    removed the sesitive info

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
+
 cloudinary.config({
-  cloud_name: 'dez41esfq',  // Replace with your Cloudinary Cloud Name
-  api_key: '772471848773624',        // Replace with your Cloudinary API Key
-  api_secret: '640U4hHY9Q8UAC36foBkST6O0ZY'   // Replace with your Cloudinary API Secret
+  cloud_name: process.env.CLOUD_NAME,   
+  api_key: process.env.API_KEYS,         
+  api_secret: process.env.API_SECRET   
 });
 
 module.exports = cloudinary;
